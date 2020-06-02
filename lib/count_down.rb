90883d1ae78743acf2f92250e50e9d2ea2153798
Seth Starr <starrseth@hotmail.com>
2020-06-02 20:01:22 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,6 @@
 magic_exit_number = 7
 count = 0
-while count < 10 && count != majic_exit_number do 
+while count < 10 && count != magic_exit_number do 
   break if count == magic_exit_number
   puts "I am the #{count}, I love to count!"
   
